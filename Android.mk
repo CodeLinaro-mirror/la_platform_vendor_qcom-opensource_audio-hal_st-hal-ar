293fdef05aa6f958d911e6a95370f434a450fdc9
Fan Zhang <quic_fanzhan@quicinc.com>
2022-06-29 17:06:59 +0530

    sthal-pal: Rename STHAL lib to use .ar suffix
    Append ".ar" to the sound_trigger.primary lib to differentiate it from the Elite STHAL lib.
    Restrict this change to msmnile_au
    
    Change-Id: Ie7dc840f37d25c5b0c4f779a5820d1295551a60c

--- Android.mk
+++ Android.mk
@@ -46,7 +46,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_LSM_HIDL)),true)
     LOCAL_CFLAGS += -DLSM_HIDL_ENABLED
 endif
 
-ifeq ($(PRODUCT_NAME), msmnile_gvmq)
+ifneq (,$(filter $(PRODUCT_NAME), msmnile_gvmq msmnile_au))
 LOCAL_ARM_MODE             := arm
 LOCAL_MODULE               := sound_trigger.primary.$(TARGET_BOARD_PLATFORM).ar
 LOCAL_MODULE_TAGS          := optional
